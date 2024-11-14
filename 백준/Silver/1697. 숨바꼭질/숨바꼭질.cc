7e6c8fe1c3a0a144c8e95b9d59ede9eb4c86e0db
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2024-11-15 00:33:48 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2932 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dis[200002];
+int n, k;
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  fill(dis, dis+200001,-1);
+
+  cin >> n >> k;
+  dis[n] = 0;
+  queue<int> Q;
+  Q.push(n);
+
+  while(dis[k] == -1){
+    auto cur = Q.front();
+    Q.pop();
+
+    for(int move : {cur-1, cur+1, 2*cur}){
+      if(dis[move] != -1) continue;
+      if(move < 0 || move > 100000) continue;
+      dis[move] = dis[cur] + 1;
+      Q.push(move);
+    }  
+  }
+
+  cout << dis[k];
+
+  int count = 0;
+  
+}

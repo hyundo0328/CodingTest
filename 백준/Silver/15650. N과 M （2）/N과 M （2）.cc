6748bbdf099b3dd8b8aace910c4d7d393984d3aa
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2024-12-23 23:00:22 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,24 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,m;
+int n, m;
 int arr[9];
-bool isused[9];
+bool isUsed[9];
 
-void func(int k){
-  if(k == m){ 
-    for(int i = 0; i < m; i++)
-      cout << arr[i] << ' '; 
+void func(int num){
+  if(num == m){
+    for(int i=0; i<m; i++){
+      cout << arr[i] << ' ';
+    }
+
     cout << '\n';
     return;
   }
 
-  int st = 1;
-  if(k != 0) st = arr[k-1] + 1;
-  
-  for(int i = st; i <= n; i++){ 
-    arr[k] = i;
-    func(k+1);
+  int start;
+  if(num != 0) {
+    start = arr[num-1] + 1;
+  } else {
+    start = 1;
+  }
+
+  for(int i=start; i<=n; i++){
+    if(!isUsed[i]){
+      arr[num] = i;
+      isUsed[i] = true;
+
+      func(num+1);
+
+      isUsed[i] = false;
+    }
   }
 }
 
@@ -27,11 +39,5 @@ int main(void){
   cin.tie(0);
 
   cin >> n >> m;
-  if(n == m){
-    for(int i=1; i<=n; i++){
-      cout << i << ' ';
-    }
-    return 0;
-  }
   func(0);
 }

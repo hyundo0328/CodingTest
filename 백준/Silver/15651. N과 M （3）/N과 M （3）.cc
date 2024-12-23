a5fa6c209631bb338842510d3612911f909d84a8
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2024-12-23 23:08:30 +0900

    [Silver III] Title: N과 M (3), Time: 340 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -1,27 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,m;
+int n, m;
 int arr[9];
-bool isused[9];
 
-void func(int k){
-  if(k == m){
-    for(int i = 0; i < m; i++)
+void func(int num){
+  if(num == m){
+    for(int i=0; i<m; i++){
       cout << arr[i] << ' ';
+    }
+
     cout << '\n';
     return;
   }
 
-  for(int i = 1; i <= n; i++){
-    arr[k] = i;
-    func(k+1);
+  for(int i=1; i<=n; i++){
+    arr[num] = i;
+    func(num+1);
   }
 }
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
+
   cin >> n >> m;
   func(0);
 }

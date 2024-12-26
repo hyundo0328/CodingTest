69b8f635ca99951765d265df41d44b76c2e6a909
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2024-12-26 23:24:47 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
 int arr[100001];
+// vector<int> vec;
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
+  int n, m;
   cin >> n;
   for(int i=0;i<n;i++){
-    int tmp;
     cin >> arr[i];
   }
   sort(arr, arr+n);

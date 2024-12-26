3e3331c9458c1b5878444f35dbbb0a2d7c04de2a
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2024-12-26 09:48:41 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -11,10 +11,11 @@ int main(void) {
   cin >> num;
 
   d[1] = 0;
-  for(int i = 2; i <= num; i++){
+  for(int i=2;i<=num;i++){
     d[i] = d[i-1]+1;
-    if(i%2 == 0) d[i] = min(d[i],d[i/2]+1);
-    if(i%3 == 0) d[i] = min(d[i],d[i/3]+1);
+    if(i%2==0) d[i] = min(d[i], d[i/2]+1);
+    if(i%3==0) d[i] = min(d[i], d[i/3]+1);
   }
+
   cout << d[num];
 }

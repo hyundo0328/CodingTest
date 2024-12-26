6a0a39b0d7aeed482e56bb5ce73933568c8d75bd
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2024-12-26 21:20:53 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -9,22 +9,20 @@ int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  cin >> n;
-  for(int i = 1; i <= n; i++) cin >> s[i];
-  if(n == 1){
-    cout << s[1];
-    return 0;
+  int t;
+  cin >> t;
+
+  for(int i=1;i<=t;i++){
+    cin >> s[i];
   }
-
   d[1][1] = s[1];
-  d[1][2] = 0;
   d[2][1] = s[2];
-  d[2][2] = s[1]+s[2];
+  d[2][2] = s[1] + s[2];
 
-  for(int i = 3; i <= n; i++){
-    d[i][1] = max(d[i-2][1],d[i-2][2])+s[i];
+  for(int i=3;i<=t;i++){
+    d[i][1] = max(d[i-2][1], d[i-2][2])+s[i];
     d[i][2] = d[i-1][1]+s[i];
   }
-  
-  cout << max(d[n][1],d[n][2]);
+
+  cout << max(d[t][1], d[t][2]);
 }

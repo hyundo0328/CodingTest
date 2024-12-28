ffd8c9039ee8e8e5c4a87d62af8796f2a3d92483
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2024-12-28 20:12:10 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+
+  vector<int> vec;
+
+  int num;
+  for(int i=0;i<n;i++){
+    cin >> num;
+    vec.push_back(num);
+  }
+  sort(vec.begin(),vec.end());
+
+  for(int number:vec){
+    cout << number << '\n';
+  }
+}

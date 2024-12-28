fd2ca3e2b59cadad37184ba89e3e7ba81274339a
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2024-12-28 14:41:35 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 3128 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,22 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
-
-  int dx[4] = {1,0,-1,0};
-  int dy[4] = {0,1,0,-1};
 
   int n, m;
   cin >> n >> m;
 
-  int board[502][502] = {0,};
-  bool vis[502][502] = {false,};
-  queue<pair<int,int> > Q;
-
   int count = 0;
   int width = 0;
+
+  int board[502][502];
+  bool vis[502][502];
+  int dx[4] = {1,0,-1,0};
+  int dy[4] = {0,1,0,-1};
 
   for(int i=0;i<n;i++){
     for(int j=0;j<m;j++){
@@ -24,34 +22,33 @@ int main(void){
     }
   }
 
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){ 
-      if(board[i][j] == 0 || vis[i][j]) continue; 
-      count++; 
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(board[i][j] == 0 || vis[i][j]) continue;
+      count++;
 
-      queue<pair<int,int> > Q;
-      vis[i][j] = true; 
+      queue<pair<int,int>> Q;
+      vis[i][j] = true;
       Q.push({i,j});
-      int area = 0; 
-      while(!Q.empty()){
-        area++; 
-        pair<int,int> cur = Q.front();
-        Q.pop();
-        for(int dir = 0; dir < 4; dir++){ 
-          int nx = cur.first + dx[dir];
-          int ny = cur.second + dy[dir];
-          
-          if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue; 
-          if(vis[nx][ny] || board[nx][ny] != 1) continue; 
 
-          vis[nx][ny] = true; 
-          Q.push({nx,ny});
+      int area = 0;
+      while(!Q.empty()){
+        area++;
+        pair<int,int> cur = Q.front(); Q.pop();
+        for(int k=0;k<4;k++){
+          int nextX = cur.first + dx[k];
+          int nextY = cur.second + dy[k];
+
+          if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) continue;
+          if(board[nextX][nextY] == 0 || vis[nextX][nextY]) continue;
+
+          vis[nextX][nextY] = true;
+          Q.push({nextX, nextY});
         }
       }
-      width = max(width, area); 
+      width = max(area, width);
     }
   }
-  cout << count << "\n" << width;
 
-  return 0;
+  cout << count << '\n' << width;
 }

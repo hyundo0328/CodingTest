629887e67a60c3ad45eac3ce83d12df79247a39d
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2024-12-28 17:01:20 +0900

    [Gold V] Title: 토마토, Time: 80 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,62 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int board[1001][1001];
+int dis[1001][1001];
+
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 
-int board[1002][1002];
-int dis[1002][1002];
-
-int n, m;
-
-int main(void){
+int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
+  int m, n;
   cin >> m >> n;
+
   queue<pair<int,int>> Q;
 
   for(int i=0;i<n;i++){
     for(int j=0;j<m;j++){
       cin >> board[i][j];
-      if(board[i][j] == 1)
+      if(board[i][j] == 1){
         Q.push({i,j});
+        dis[i][j] = 0;
+      }
       if(board[i][j] == 0)
         dis[i][j] = -1;
     }
   }
-  
+
   while(!Q.empty()){
-    auto cur = Q.front();
-    Q.pop();    
+    pair<int,int> cur = Q.front(); Q.pop();
+    for(int k=0;k<4;k++){
+      int nextX = cur.first + dx[k];
+      int nextY = cur.second + dy[k];
 
-    for(int i=0;i<4;i++){
-      int nx = cur.first + dx[i];
-      int ny = cur.second + dy[i];
-
-      if(nx < 0 || ny < 0 || nx >= n || ny >= m) {
-        continue;
-      }
-      if(dis[nx][ny] >= 0) {
+      if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) continue;
+      if(dis[nextX][nextY] >= 0) {
         continue;
       }
 
-      dis[nx][ny] = dis[cur.first][cur.second] + 1;
-      Q.push({nx,ny});
+      dis[nextX][nextY] = dis[cur.first][cur.second]+1;
+      Q.push({nextX, nextY});
     }
   }
 
-  int day = 0;
+  int max_day = 0;
   for(int i=0;i<n;i++){
     for(int j=0;j<m;j++){
       if(dis[i][j] == -1){
         cout << -1;
         return 0;
       }
-      day = max(day, dis[i][j]);
+      max_day = max(max_day, dis[i][j]);
     }
   }
-  cout << day;
-
-  return 0;
+  cout << max_day;
 }

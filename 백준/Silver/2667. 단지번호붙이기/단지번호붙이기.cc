206877211c16bcfa1f204653a15bb3a4012bac06
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-01-23 01:22:09 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,65 +1,65 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
 int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
+int dy[4] = {0,-1,0,1};
 
 int board[27][27];
-int vis[27][27];
-int n;
+bool vis[27][27];
 
-int main(void){
+int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
+  int n;
   cin >> n;
+
   for(int i=0;i<n;i++){
-    string tmp;
-    cin >> tmp;
+    string s;
+    cin >> s;
     for(int j=0;j<n;j++){
-      board[i][j] = tmp[j]-'0';
+      board[i][j] = s[j] - '0';
     }
   }
 
   int count = 0;
   vector<int> ans;
 
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      if (board[i][j] == 0 || vis[i][j] == 1) continue;
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+      if(board[i][j] == 0 || !!vis[i][j]) continue;
 
-      queue<pair<int, int>> Q;
-      Q.push({ i, j });
-      vis[i][j] = 1;
+      queue<pair<int,int>> Q;
+      Q.push({i,j});
+      vis[i][j] = true;
       
-      int width = 1;
       count++;
+      int width = 1;
 
-      while (!Q.empty()) {
-        auto cur = Q.front();
-        Q.pop();
+      while(!Q.empty()){
+        auto cur = Q.front(); Q.pop();
 
-        for (int dir = 0; dir < 4; dir++) {
+        for(int dir=0; dir<4; dir++){
           int nx = cur.first + dx[dir];
           int ny = cur.second + dy[dir];
 
-          if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-          if (board[nx][ny] == 0 || vis[nx][ny] == 1) continue;
+          if(nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+          if(!!vis[nx][ny] || board[nx][ny] == 0) continue;
 
-          Q.push({ nx, ny });
-          vis[nx][ny] = 1;
+          Q.push({nx,ny});
+          vis[nx][ny] = true;
           width++;
         }
       }
+
       ans.push_back(width);
     }
   }
+
   sort(ans.begin(), ans.end());
 
   cout << count << '\n';
-  for (int i : ans)
-    cout << i << '\n'; 
-
-  return 0;
+  for(auto width:ans){
+    cout << width << '\n';
+  }
 }

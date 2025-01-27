468e32ec015f6ee112290f7e8938f60299f19b67
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-01-27 16:56:06 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    int idx1 = 0;
+    int idx2 = 0;
+    for(int i=0;i<goal.size();i++){
+        if(cards1[idx1] == goal[i]){
+            idx1++;
+        } else if (cards2[idx2] == goal[i]){
+            idx2++;
+        } else {
+            return "No";
+        }
+    }
+    
+    return "Yes";
+}

b4007438b4bf43faae988ae7aec7100226265f64
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-02-03 00:35:33 +0900

    [Gold II] Title: 문제 추천 시스템 Version 2, Time: 84 ms, Memory: 12676 KB -BaekjoonHub

--- 백준/Gold/21944. 문제 추천 시스템 Version 2/문제 추천 시스템 Version 2.cc
+++ 백준/Gold/21944. 문제 추천 시스템 Version 2/문제 추천 시스템 Version 2.cc
@@ -18,72 +18,66 @@ int main(void) {
     probByL[L].insert(P);
     probByGL[G][L].insert(P);
   }
+    
   cin >> N;
   while (N--) {
     cin >> op;
     if (op == "recommend") {
       cin >> G >> x;
       if (x == 1) {
-        for (int i = 100; i >= 0; i--) {
+        for (int i=100; i>=0; i--) {
           if (probByGL[G][i].empty()) continue;
           cout << *(prev(probByGL[G][i].end())) << '\n';
           break;
         }
       } else {
-        for (int i = 0; i < 101; i++) {
+        for (int i=0; i<=100; i++) {
           if (probByGL[G][i].empty()) continue;
           cout << *probByGL[G][i].begin() << '\n';
           break;
         }
       }
-    }
-    
-    else if(op == "recommend2"){
+    } else if (op == "recommend2"){
       cin >> x;
         if (x == 1) {
-        for (int i = 100; i >= 0; i--) {
+        for (int i=100; i>=0; i--) {
           if (probByL[i].empty()) continue;
           cout << *(prev(probByL[i].end())) << '\n';
           break;
         }
       } else {
-        for (int i = 0; i < 101; i++) {
+        for (int i=0; i<=100; i++) {
           if (probByL[i].empty()) continue;
           cout << *probByL[i].begin() << '\n';
           break;
         }
       }
-    }
-
-    else if(op == "recommend3"){
+    } else if (op == "recommend3"){
       cin >> x >> L;
       int ans = -1;
       if(x == 1){
-        for(int i = L; i < 101; i++){
+        for(int i=L; i<=100; i++){
           if(probByL[i].empty()) continue;
           ans = *probByL[i].begin();
           break;
         }
-      }
-      else{
-        for(int i = L-1; i >= 0; i--){
+      } else {
+        for(int i=L-1; i>=0; i--){
           if(probByL[i].empty()) continue;
           ans = *(prev(probByL[i].end()));
           break;
         }
       }
       cout << ans << '\n';
-    }
-    
-    else if (op == "add") {
+    } else if (op == "add") {
       cin >> P >> L >> G;
+        
       probLevel[P] = {L, G};
       probByL[L].insert(P);
       probByGL[G][L].insert(P);
-    }
-    
-    else if (op == "solved") {
+    } else if (op == "solved") {
       cin >> P;
+        
       tie(L, G) = probLevel[P];
       probByL[L].erase(P);
       probByGL[G][L].erase(P);

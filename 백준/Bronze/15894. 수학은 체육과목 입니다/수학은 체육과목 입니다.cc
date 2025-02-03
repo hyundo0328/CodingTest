bf6e43f5f86500e1397bec67d60455398ab88d09
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-02-04 02:21:00 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  long long n;
+  cin >> n;
+
+  long long ans = 3*n + 1 + (n-1);
+  cout << ans;
+}

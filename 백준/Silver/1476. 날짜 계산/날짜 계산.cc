30cfeb3b91ca6d5f05ab4fd640353f964170299d
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-02-04 18:08:38 +0900

    [Silver V] Title: 날짜 계산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.cc
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.cc
@@ -7,17 +7,14 @@ int main(){
 
   int e, s, m; // e: 지구(1 <= e <= 15), s: 태양(1 <= s <= 28), m: 달(1 <= m <= 19)
   cin >> e >> s >> m;
-  
+
   int year = 1;
   while(1){
-    if((year-e)%15 == 0 && (year-s)%28==0 && (year-m)%19 == 0){
-      break;
+    if((year-e)%15 == 0 && (year-s)%28 == 0 && (year-m)%19 == 0){
+      cout << year << '\n';
+      return 0;
     } else {
       year++;
     }
   }
-  
-  cout << year << "\n";
-  
-  return 0;
 }

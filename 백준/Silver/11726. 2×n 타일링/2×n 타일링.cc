9d827e45b849d02df92bdefc8b1bd283ff05d3a5
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-03-21 23:47:08 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,16 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int d[10005];
-int mod = 10007;
+int d[1002];
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
+
   int n;
   cin >> n;
-  d[1] = 1;
-  d[2] = 2;
-  for(int i = 3; i <= n; i++) d[i] = (d[i-1]+d[i-2])%mod;
+
+  d[1] = 1; d[2] = 2;
+  for(int i=3; i<=1000; i++){
+    d[i] = (d[i-1] + d[i-2]) % 10007;
+  }
+
   cout << d[n];
 }

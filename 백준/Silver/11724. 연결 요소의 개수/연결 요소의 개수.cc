78b2977ec45de8af1b87d4b34bbecc216fdf0b1c
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-05-08 20:23:46 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 68 ms, Memory: 6536 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -1,41 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> adj[1005];
-bool vis[1005];
+vector<int> adj[1002];
+bool vis[1002];
+
+void bfs(int n){
+  queue<int> Q;
+  Q.push(n);
+
+  while(!Q.empty()){
+    int cur = Q.front(); Q.pop();
+
+    for(int nxt:adj[cur]){
+      if(vis[nxt]) continue;
+
+      Q.push(nxt);
+      vis[nxt] = true;
+    }
+  }
+}
 
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  int n, m;
 
-  cin >> n >> m;  
-  while(m--){
+  int n, m;
+  cin >> n >> m;
+
+  while(m-- > 0){
     int u, v;
     cin >> u >> v;
+
     adj[u].push_back(v);
-    adj[v].push_back(u);    
+    adj[v].push_back(u);
   }
 
-  int num = 0;
-  for(int i = 1; i <= n; i++){
+  int count = 0;
+  for(int i=1; i<=n; i++){
     if(vis[i]) continue;
-    num++;
-      
-    queue<int> q;
-    q.push(i);
-      
     vis[i] = true;
-    while(!q.empty()){
-      int cur = q.front(); q.pop();
-        
-      for(auto nxt : adj[cur]){
-        if(vis[nxt]) continue;
-        q.push(nxt);
-        vis[nxt] = true;
-      }
-    }
+    count++;
+    bfs(i);
   }
-    
-  cout << num;
+
+  cout << count;
 }

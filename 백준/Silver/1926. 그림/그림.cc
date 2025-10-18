0776bafec2cf51f52f4bf96d20999e32e8684f0e
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-10-18 12:03:25 +0900

    [Silver I] Title: 그림, Time: 24 ms, Memory: 3252 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int board[502][502];
+bool vis[502][502] = {false,};
+
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
+
+int n, m;
+
+int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int n, m;
   cin >> n >> m;
-
-  int count = 0;
-  int width = 0;
-
-  int board[502][502];
-  bool vis[502][502];
-  int dx[4] = {1,0,-1,0};
-  int dy[4] = {0,1,0,-1};
 
   for(int i=0;i<n;i++){
     for(int j=0;j<m;j++){
@@ -22,33 +21,37 @@ int main(){
     }
   }
 
+  int count = 0, width = 0, mx = 0;
+
   for(int i=0;i<n;i++){
     for(int j=0;j<m;j++){
-      if(board[i][j] == 0 || vis[i][j]) continue;
-      count++;
+      if(vis[i][j] == true || board[i][j] == 0) continue;
 
-      queue<pair<int,int>> Q;
+      count++;
       vis[i][j] = true;
+      queue<pair<int, int>> Q;
       Q.push({i,j});
 
-      int area = 0;
+      width = 0;
       while(!Q.empty()){
-        area++;
-        pair<int,int> cur = Q.front(); Q.pop();
-        for(int k=0;k<4;k++){
-          int nextX = cur.first + dx[k];
-          int nextY = cur.second + dy[k];
+        width++;
+        auto cur = Q.front(); Q.pop();
 
-          if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) continue;
-          if(board[nextX][nextY] == 0 || vis[nextX][nextY]) continue;
+        for(int dir=0;dir<4;dir++){
+          int nx = cur.first + dx[dir];
+          int ny = cur.second + dy[dir];
 
-          vis[nextX][nextY] = true;
-          Q.push({nextX, nextY});
+          if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue; 
+          if(vis[nx][ny] || board[nx][ny] == 0) continue; 
+
+          vis[nx][ny] = true;
+          Q.push({nx,ny});
         }
+        
       }
-      width = max(area, width);
+      mx = max(mx, width); 
     }
   }
+  cout << count << "\n" << mx;
 
-  cout << count << '\n' << width;
 }

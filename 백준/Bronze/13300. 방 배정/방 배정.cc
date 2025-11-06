26333c5bb3ae07dd0a6e65fd5299f67581e397b2
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-11-06 18:21:58 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.cc
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int students[7][2];
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
@@ -8,21 +10,21 @@ int main() {
 	int n, k;
 	cin >> n >> k;
 
-  int num[2][6] = {0,};
-  for(int i=0;i<n;i++){
-    int s, y;
-    cin >> s >> y;
-    num[s][y-1]++;
+  for(int i=0; i<n; i++){
+    int gender, grade;
+    cin >> gender >> grade;
+
+    students[grade][gender]++;
   }
 
   int count = 0;
-  for(int i=0;i<6;i++){
-    count += (num[0][i])/k;
-    count += (num[1][i])/k;
-    if(num[0][i]%k != 0) count++;
-    if(num[1][i]%k != 0) count++;
+  for(int i=1; i<=6; i++){
+    int boy = students[i][1];
+    int girl = students[i][0];
+
+    count += (boy%k == 0 ? boy/k : boy/k+1);
+    count += (girl%k == 0 ? girl/k : girl/k+1);
   }
+
   cout << count;
-	
-  return 0;
 }

bf6146a4c879bc7af8909b86b380c17ecec92a9e
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-11-06 17:12:50 +0900

    [Silver V] Title: 방 번호, Time: 100 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,28 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-    int n; n = Integer.parseInt(br.readLine());
+        int[] cnt = new int[10];
 
-    int[] num = new int[10];
-    int count = 0;
-    while(n > 0){
-      if(n%10 == 6 || n%10 == 9){
-        if(num[6] > num[9]) num[9]++;
-        else num[6]++;
-      } else {
-        num[n%10]++;
-      }
+        while(n > 0){
+            int num = n%10;
 
-      n /= 10;
+            if(num == 6 || num == 9){
+                if(cnt[6] >= cnt[9]) cnt[9]++;
+                else cnt[6]++;
+            } else {
+                cnt[num]++;
+            }
+
+            n /= 10;
+        }
+
+        int max = -1;
+        for(int count:cnt){
+            if(max <= count) max = count;
+        }
+        
+        System.out.println(max);
     }
 
-    System.out.println(Arrays.stream(num).max().getAsInt());
-  }
 }

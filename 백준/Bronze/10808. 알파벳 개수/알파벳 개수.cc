c9a5f56d41a15b4728a3380561509fdaf8b7091c
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-11-06 15:43:46 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,18 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int alphabet[26];
+
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int alpha[26] = {0,};
-  string s;
-  cin >> s;
-  for(auto c : s){
-    alpha[c-'a']++;
-  }
-  for(int i = 0; i < 26; i++)
-    cout << alpha[i] << ' ';
+  string str;
+  cin >> str;
 
-  return 0;
+  for(int i=0; i<str.length(); i++) alphabet[str.at(i) - 'a']++;
+
+  for(auto count:alphabet) cout << count << " ";
+  
 }

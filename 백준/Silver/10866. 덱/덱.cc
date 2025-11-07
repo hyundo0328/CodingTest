ed1cea329959189867c1349ba93f52f3325233d4
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-11-07 12:21:53 +0900

    [Silver IV] Title: 덱, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.cc
+++ 백준/Silver/10866. 덱/덱.cc
@@ -5,19 +5,18 @@ int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  deque<int> dq;
   int n;
   cin >> n;
+
+  deque<int> dq;
   while (n--) {
     string q;
     cin >> q;
     if (q == "push_back") {
-      int val;
-      cin >> val;
+      int val; cin >> val;
       dq.push_back(val);
     } else if (q == "push_front") {
-      int val;
-      cin >> val;
+      int val; cin >> val;
       dq.push_front(val);
     } else if(q == "pop_front"){
       if(dq.empty()) cout << -1 << '\n';
@@ -31,16 +30,10 @@ int main(void) {
         cout << dq.back() << '\n';
         dq.pop_back();
       }
-    } else if (q == "size"){
-      cout << dq.size() << '\n';
-    } else if (q == "empty"){
-      cout << dq.empty() << '\n';
-    } else if (q == "front") {
-      if(dq.empty()) cout << -1 << '\n';
-      else cout << dq.front() << '\n';
-    } else {
-      if(dq.empty()) cout << -1 << '\n';
-      else cout << dq.back() << '\n';
-    }
+    } 
+    else if (q == "size") cout << dq.size() << '\n';
+    else if (q == "empty") cout << dq.empty() << '\n';
+    else if (q == "front") dq.empty() ? cout << -1 << '\n' : cout << dq.front() << '\n';
+    else if (q == "back") dq.empty() ? cout << -1 << '\n' : cout << dq.back() << '\n';
   }
 }

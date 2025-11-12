ed1852b7979b974c71f2894b858e3cbee83f7b2d
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-11-12 15:42:58 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2488 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -1,24 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, ans = 0;
-
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
+  int n;
   cin >> n;
-  while (n--) {
-    string a;
-    cin >> a;
-      
-    stack<int> S;
-    for (auto c : a) {
-      if (!S.empty() && S.top() == c) S.pop();
-      else S.push(c);
+
+  int count = 0;
+  while(n--){
+    string str;
+    cin >> str;
+
+    stack<char> s;
+    bool flag = false;
+    for(char c:str){
+      if (!s.empty() && s.top() == c) s.pop();
+      else s.push(c);
     }
-      
-    if (S.empty()) ans++;
+
+    if(s.empty()) count++;
   }
-  cout << ans << "\n";
+
+  cout << count;
 }

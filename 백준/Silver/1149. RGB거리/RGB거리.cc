aee61f573c86e775b42808dd772e667322c6833b
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-11-20 12:21:58 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,26 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int r[1005], g[1005], b[1005];
-int d[1005][3];
+int r[1002];
+int g[1002];
+int b[1002];
+int house[1002][4];
 
-int main(void){
+int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int t;
-  cin >> t;
+  int n;
+  cin >> n;
 
-  for(int i=1;i<=t;i++){
-    cin >> r[i] >> g[i] >> b[i];
-  }
-  d[1][0] = r[1]; d[1][1] = g[1]; d[1][2] = b[1];
-
-  for(int i=2;i<=t;i++){
-    d[i][0] = min(d[i-1][1], d[i-1][2])+r[i];
-    d[i][1] = min(d[i-1][0], d[i-1][2])+g[i];
-    d[i][2] = min(d[i-1][0], d[i-1][1])+b[i];
+  for(int i=1; i<=n; i++){
+    cin >> r[i];
+    cin >> g[i];
+    cin >> b[i];
   }
 
-  cout << min({d[t][0], d[t][1], d[t][2]});
+  house[1][1] = r[1]; // 빨간색 시작
+  house[1][2] = g[1]; // 초록색 시작
+  house[1][3] = b[1]; // 파란색 시작
+
+  for(int i=2; i<=n; i++){
+    house[i][1] = min(house[i-1][2], house[i-1][3]) + r[i];
+    house[i][2] = min(house[i-1][1], house[i-1][3]) + g[i];
+    house[i][3] = min(house[i-1][1], house[i-1][2]) + b[i];
+  }
+
+  cout << min({house[n][1], house[n][2], house[n][3]});
 }

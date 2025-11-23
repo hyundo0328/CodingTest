a5aca81a45bb296d2818e22b91b03c3eef9b4c19
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-11-24 00:40:04 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 3988 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -10,15 +10,17 @@ int main(){
 
   int n;
   cin >> n;
-  
-  for(int i=1;i<=n;i++)
-    for(int j=1;j<=i;j++)
+
+  for(int i=1; i<=n; i++){
+    for(int j=1; j<=i; j++)
       cin >> num[i][j];
+  }
 
   dp[1][1] = num[1][1];
-  for(int i=2; i<=n; i++)
+  for(int i=2; i<=n; i++){
     for(int j=1; j<=i; j++)
-      dp[i][j] = max(dp[i-1][j-1],dp[i-1][j]) + num[i][j];
+      dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + num[i][j];
+  }
 
   cout << *max_element(dp[n]+1, dp[n]+n+1);
 }

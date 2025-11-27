7c3c0f1603636b35bac783a65bad86364aa972bb
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-11-27 09:45:56 +0900

    [Silver IV] Title: 수 찾기, Time: 44 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,24 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[100001];
-// vector<int> vec;
+vector<int> vec;
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int n, m;
+  int n;
   cin >> n;
-  for(int i=0;i<n;i++){
-    cin >> arr[i];
-  }
-  sort(arr, arr+n);
 
-  cin >> m;
-  for(int i=0;i<m;i++){
-    int tmp;
-    cin >> tmp;
-    cout << binary_search(arr, arr+n, tmp) << '\n';
+  for(int i=0; i<n; i++){
+    int num; cin >> num;
+    vec.push_back(num);
+  }
+
+  sort(vec.begin(), vec.end());
+
+  cin >> n;
+  for(int i=0; i<n; i++){
+    int num; cin >> num;
+    cout << binary_search(vec.begin(), vec.end(), num) << '\n';
   }
 }

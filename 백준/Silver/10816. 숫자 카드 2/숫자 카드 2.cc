0e40a0306743d2aac76eeaff70d6444deb68dbaf
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-11-30 20:43:32 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 304 ms, Memory: 12204 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,28 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-// int arr[100001];
-vector<int> vec;
+long long arr[500001];
+vector<long long> vec;
 
-int main(void){
+int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  cin >> n;
-  for(int i=0;i<n;i++){
-    int tmp;
-    cin >> tmp;
+  int n; cin >> n;
+
+  for(int i=0; i<n; i++){
+    long long tmp; cin >> tmp;
     vec.push_back(tmp);
   }
-  // sort(arr, arr+n);
-  sort(vec.begin(),vec.end());
+  sort(vec.begin(), vec.end());
 
-  cin >> m;
-  for(int i=0;i<m;i++){
-    int tmp;
-    cin >> tmp;
-    // cout << upper_bound(arr, arr+n, tmp)-lower_bound(arr, arr+n, tmp) << '\n';
-    cout << upper_bound(vec.begin(),vec.end(),tmp)-lower_bound(vec.begin(),vec.end(),tmp) << '\n';
+  int m; cin >> m;
+  while(m--){
+    long long tmp; cin >> tmp;
+
+    cout << upper_bound(vec.begin(), vec.end(), tmp) - lower_bound(vec.begin(), vec.end(), tmp) << " ";
   }
+
 }

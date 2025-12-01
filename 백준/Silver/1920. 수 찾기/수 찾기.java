275cbdbfb3646231883af6cf6fda55f2e7bf7e77
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-01 11:32:45 +0900

    [Silver IV] Title: 수 찾기, Time: 1060 ms, Memory: 53140 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -6,43 +6,25 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-  public static int n;
-  public static int[] arr;
-
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = null;
 
-    n = Integer.parseInt(br.readLine());
-    st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
 
-    arr = new int[n];
-    for(int i=0; i<n; i++){
-      arr[i] = Integer.parseInt(st.nextToken());
-    }
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    for(int i=0; i<n; i++) arr[i] = Integer.parseInt(st.nextToken());
     Arrays.sort(arr);
 
     int m = Integer.parseInt(br.readLine());
-    st = new StringTokenizer(br.readLine());
+    st = new StringTokenizer(br.readLine(), " ");
     while(m-- > 0){
-      int num = Integer.parseInt(st.nextToken());
+      int tmp = Integer.parseInt(st.nextToken());
 
-      System.out.println(binarySearch(num));
+      int result = Arrays.binarySearch(arr, tmp);
+      if(result >= 0) System.out.println(1);
+      else System.out.println(0);
     }
   }
 
-  public static int binarySearch(int target){
-    int st = 0;
-    int en = n-1;
-    while(st <= en){
-      int mid = (st+en)/2;
-      if(arr[mid] < target)
-        st = mid+1;
-      else if(arr[mid] > target)
-        en = mid-1;
-      else
-        return 1;
-    }
-    return 0;
-  }
 }

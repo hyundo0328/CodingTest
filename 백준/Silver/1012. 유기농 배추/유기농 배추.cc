9280d8a116a3cd77ff1797d9bb540a45521bd3ed
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-02 12:17:44 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,65 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int t;
-int n, m, k;
-
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
-
 int board[52][52];
-int vis[52][52];
+bool vis[52][52];
+int n, m, k;
+int cnt = 0;
+queue<pair<int, int>> q;
 
-int main(void){
+int bfs(){
+  int width = 0;
+  while(!q.empty()){
+    auto cur = q.front(); q.pop();
+
+    for(int dir=0; dir<4; dir++){
+      int nx = cur.first + dx[dir];
+      int ny = cur.second + dy[dir];
+
+      if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+      if(board[nx][ny] == 0 || vis[nx][ny]) continue;
+
+      width++;
+      vis[nx][ny] = true;
+      q.push({nx, ny});
+    }
+  }
+
+  return width;
+}
+
+void area(){
+  for(int i=0; i<n; i++){
+    for(int j=0; j<m; j++){
+      if(board[i][j] == 0 || vis[i][j]) continue;
+      
+      cnt++;
+      board[i][j] = true;
+      q.push({i, j});
+
+      int width = bfs();
+    }
+  }
+
+  cout << cnt << '\n';
+}
+
+int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  cin >> t;
-  while(t-- > 0){
+  int t; cin >> t;
+  while(t--){
     cin >> n >> m >> k;
-    for(int i=0;i<n;i++){
+
+    // 초기화
+    cnt = 0; q = queue<pair<int, int>>();
+    for(int i=0; i<n; i++){
       fill(board[i], board[i]+m, 0);
-      fill(vis[i], vis[i]+m, 0);
+      fill(vis[i], vis[i]+m, false);
     }
 
-    for(int i=0;i<k;i++){
+    while(k--){
       int x, y;
       cin >> x >> y;
+
       board[x][y] = 1;
     }
 
-    int count = 0;
-    for(int i=0;i<n;i++){
-      for(int j=0;j<m;j++){
-        if(vis[i][j] == 1 || board[i][j] == 0) continue;
-        
-        queue<pair<int,int>> Q;
-        vis[i][j] == 1;
-        Q.push({i,j});
-        count++;
-
-        while(!Q.empty()){
-          auto cur = Q.front();
-          Q.pop();    
-
-          for(int i=0;i<4;i++){
-            int nx = cur.first + dx[i];
-            int ny = cur.second + dy[i];
-
-            if(nx < 0 || ny < 0 || nx >= n || ny >= m) {
-              continue;
-            }
-            if(vis[nx][ny] == 1 || board[nx][ny] == 0) {
-              continue;
-            }
-
-            vis[nx][ny] = 1;
-            Q.push({nx,ny});
-          }
-        }
-      }
-    }
-
-    cout << count << '\n';
+    area();
   }
 }

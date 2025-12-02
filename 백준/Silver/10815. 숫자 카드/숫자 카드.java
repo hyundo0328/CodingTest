476594b32f5877c64f3689b9f84402f7c1d900b2
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-02 11:16:43 +0900

    [Silver V] Title: 숫자 카드, Time: 3124 ms, Memory: 145944 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()){
+            int tmp = Integer.parseInt(st.nextToken());
+            list.add(tmp);
+        }
+        Collections.sort(list);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()){
+            int tmp = Integer.parseInt(st.nextToken());
+
+            if(Collections.binarySearch(list, tmp) >= 0) System.out.print(1 + " ");
+            else System.out.print(0 + " ");
+        }
+    }
+
+}

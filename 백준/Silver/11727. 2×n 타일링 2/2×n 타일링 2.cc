6142726c649e2dc3f3ae62e9291658d934f95c2a
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-04 00:35:53 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -10,11 +10,8 @@ int main(){
   int n;
   cin >> n;
 
-  dp[1] = 1; dp[2] = 3;
+  dp[1] = 1; dp[2] = 3;;
+  for(int i=3; i<=1000; i++) dp[i] = (dp[i-1] + dp[i-2]*2) % 10007;
   
-  for(int i=3; i<=1000; i++){
-    dp[i] = (dp[i-1] + dp[i-2] * 2)%10007;
-  }
-
   cout << dp[n];
 }

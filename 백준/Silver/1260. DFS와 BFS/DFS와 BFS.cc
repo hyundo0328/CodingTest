82017393114fde051801fdfdd024c63b5ebaad87
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-03 09:50:41 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,30 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, v;
 vector<int> adj[1002];
 bool vis[1002];
 
 void dfs(int cur){
   vis[cur] = true;
-  cout << cur << ' ';
-
-  for(auto nxt: adj[cur]){
+  cout << cur << " ";
+  for(int nxt:adj[cur]){
     if(vis[nxt]) continue;
+
     dfs(nxt);
   }
 }
 
-void bfs(int n){
+void bfs(int num){
   queue<int> q;
-  q.push(n);
-  vis[n] = true;
-
+  q.push(num);
+  vis[num] = true;
+  
   while(!q.empty()){
-    auto cur = q.front(); q.pop();
-    cout << cur << ' ';
+    int cur = q.front(); q.pop();
 
-    for(auto nxt : adj[cur]){
+    cout << cur << " ";
+    for(int nxt:adj[cur]){
       if(vis[nxt]) continue;
 
       vis[nxt] = true;
@@ -37,22 +36,22 @@ int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
+  int n, m, v;
   cin >> n >> m >> v;
-  int a, b;
-  for(int i=0;i<m;i++){
-    cin >> a >> b;
-    adj[a].push_back(b);
-    adj[b].push_back(a);
-  }
 
-  for(int i = 1; i <= n; i++){
-    sort(adj[i].begin(), adj[i].end());
+  while(m--){
+    int u, v;
+    cin >> u >> v;
+
+    adj[u].push_back(v);
+    adj[v].push_back(u);
   }
+  
+  for(int i=1; i<=n; i++) sort(adj[i].begin(), adj[i].end());
 
   dfs(v);
-
   cout << '\n';
-  fill(vis, vis+n+1, false);
-  
+
+  fill(vis, vis+1002, false);
   bfs(v);
 }

b0a28e983be1c17f8d55dad514c9fb1cbaccb134
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-11 17:52:38 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1520 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,21 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, num;
-int arr[10001];
+int num_arr[10002];
 
-int main(void) {
+int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  cin >> n;
-  for(int i = 0; i < n; i++){
-    cin >> num; arr[num]++;
+  int n; cin >> n;
+  while(n--){
+    int tmp; cin >> tmp;
+    num_arr[tmp]++;
   }
 
-  for(int i = 1; i <= 10000; i++){
-    while(arr[i]){
-      cout << i << '\n'; arr[i]--;
-    }
+  for(int i=1; i<=10000; i++){
+    while(num_arr[i]-- > 0) cout << i << '\n';
   }
 }

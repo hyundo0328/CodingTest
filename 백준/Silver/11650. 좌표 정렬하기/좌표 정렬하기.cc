57ea760ddec2db129d7475c11d1952dc45e925bf
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-13 17:41:05 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -5,20 +5,16 @@ int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int n;
-  cin >> n;
+  int n; cin >> n;
 
   vector<pair<int, int>> vec;
 
-  int x;
-  int y;
   for(int i=0;i<n;i++){
+    int x, y;
     cin >> x >> y;
     vec.push_back({x, y});
   }
   sort(vec.begin(),vec.end());
 
-  for(auto point:vec){
-    cout << point.first << ' ' << point.second << '\n';
-  }
+  for(auto point:vec) cout << point.first << ' ' << point.second << '\n';
 }

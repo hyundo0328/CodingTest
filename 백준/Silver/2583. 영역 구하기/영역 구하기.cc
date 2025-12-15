e24adbbcc7c7c8aa28ef624d440d5d4cc9892a94
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-16 01:05:53 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -3,9 +3,8 @@ using namespace std;
 
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
-
 int board[102][102];
-int vis[102][102];
+bool vis[102][102];
 int m, n, k;
 
 int main(void){
@@ -15,41 +14,38 @@ int main(void){
   int m, n, k;
   cin >> m >> n >> k;
 
-  while(k-- > 0){
+  while(k--){
     int a, b, c, d;
     cin >> a >> b >> c >> d;
-    for(int i=b;i<d;i++){
-      for(int j=a;j<c;j++){
-        board[i][j] = 1;
-      }
+    for(int i=b; i<d; i++){
+      for(int j=a; j<c; j++) board[i][j] = 1;
     }
   }
 
   int count = 0;
   vector<int> ans;
 	
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
-      if (board[i][j] == 1 || vis[i][j] == 1) continue;
+  for (int i=0; i<m; i++) {
+    for (int j=0; j<n; j++) {
+      if (board[i][j] == 1 || vis[i][j]) continue;
 
       queue<pair<int, int>> Q;
       vis[i][j] = 1;
-      Q.push({ i, j });
+      Q.push({i, j});
 
       int width = 1;
       count++;
       while (!Q.empty()) {
-        auto cur = Q.front();
-        Q.pop();
+        auto cur = Q.front(); Q.pop();
 
-        for (int dir = 0; dir < 4; dir++) {
+        for (int dir=0; dir<4; dir++) {
           int nx = cur.first + dx[dir];
           int ny = cur.second + dy[dir];
 
           if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-          if (board[nx][ny] == 1 || vis[nx][ny] == 1) continue;
+          if (board[nx][ny] == 1 || vis[nx][ny]) continue;
           
-          Q.push({ nx, ny });
+          Q.push({nx, ny});
           vis[nx][ny] = 1;
           width++;
         }
@@ -57,13 +53,8 @@ int main(void){
       ans.push_back(width);
     }
   }
-    
   sort(ans.begin(), ans.end());
 
   cout << count << '\n';
-  for (int i : ans){
-    cout << i << ' '; 
-  }
-
-  return 0;
+  for (int width : ans) cout << width << ' '; 
 }

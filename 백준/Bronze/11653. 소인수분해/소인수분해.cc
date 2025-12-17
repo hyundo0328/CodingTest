0912f285f971fdae33163096e942a2293df78fdb
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-17 20:08:13 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -4,8 +4,9 @@ using namespace std;
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  int n;
-  cin >> n;
+
+  int n; cin >> n;
+  
   for(int i = 2; i*i <= n; i++){
     while(n % i == 0){
       cout << i << '\n';

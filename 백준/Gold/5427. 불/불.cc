396e6cb3f2c2e6b4975702553ced78dd7220ce7a
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-17 17:15:33 +0900

    [Gold IV] Title: 불, Time: 88 ms, Memory: 12984 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.cc
+++ 백준/Gold/5427. 불/불.cc
@@ -3,90 +3,90 @@ using namespace std;
 
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
+char board[1002][1002];
+int disF[1002][1002];
+int disS[1002][1002];
+int n, m, t;
+bool flag;
+queue<pair<int,int>> qf;
+queue<pair<int,int>> qs;
 
-int bX, bY;
-int board[1002][1002];
-int visF[1002][1002];
-int visS[1002][1002];
+void init(){
+  for(int i=0; i<1002; i++){
+    fill(disF[i], disF[i] + 1002, -1);
+    fill(disS[i], disS[i] + 1002, -1);
+  }
+
+  flag = false;
+  qf = {}; qs = {};
+}
+
+void input(){
+  for(int i=0; i<n; i++){
+    for(int j=0; j<m; j++){
+      cin >> board[i][j];
+      if(board[i][j] == '@'){
+        disS[i][j] = 0;
+        qs.push({i,j});
+      }
+
+      if(board[i][j] == '*'){
+        disF[i][j] = 0;
+        qf.push({i,j});
+      }
+    }
+  }
+}
 
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int c;
-  cin >> c;
+  cin >> t;
+  
+  while(t--){
+    cin >> m >> n;
 
-  while(c-- > 0){
-    bool escape = false;
-    cin >> bY >> bX;
+    init();
+    input();
 
-    queue<pair<int,int>> Sanguen;
-    queue<pair<int,int>> Fire;
+    // 불의 이동거리
+    while(!qf.empty()){
+      auto cur = qf.front(); qf.pop();
 
-    for (int i = 0; i < bX; i++) {
-      fill(visF[i], visF[i] + bY, 0);
-      fill(visS[i], visS[i] + bY, 0);
-    }
-    for (int i = 0; i < bX; i++){
-      for (int j = 0; j < bY; j++) {
-        char c;
-        cin >> c;
-        if (c == '#') board[i][j] = -1;
-        else {
-          if (c == '@') {
-            Sanguen.push({i, j});
-            visS[i][j] = 1;
-          } 
-          else if (c == '*') {
-            Fire.push({i, j});
-            visF[i][j] = 1;
-          }
-          board[i][j] = 0;
-        }
+      for(int dir=0; dir<4; dir++){
+        int nx = cur.first + dx[dir];
+        int ny = cur.second + dy[dir];
+
+        if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+        if(board[nx][ny] == '#' || disF[nx][ny] != -1) continue;
+
+        disF[nx][ny] = disF[cur.first][cur.second] + 1;
+        qf.push({nx,ny});
       }
     }
 
+    // 상근 이동거리
+    while(!qs.empty() && !flag){
+      auto cur = qs.front(); qs.pop();
 
-    while(!Fire.empty()){
-      auto cur = Fire.front();
-      Fire.pop();
+      for(int dir=0; dir<4; dir++){
+        int nx = cur.first + dx[dir];
+        int ny = cur.second + dy[dir];
 
-      for(int i=0;i<4;i++){
-        int nx = cur.first + dx[i];
-        int ny = cur.second + dy[i];
-
-        if(nx < 0 || nx >= bX || ny < 0 || ny >= bY) {
-          continue;
-        }
-        if(visF[nx][ny] > 0 || board[nx][ny] == -1) {
-          continue;
-        }
-
-        visF[nx][ny] = visF[cur.first][cur.second] + 1;
-        Fire.push({nx,ny});
-      }
-    }
-
-    while ((!Sanguen.empty()) && (!escape)) {
-      auto cur = Sanguen.front();
-      Sanguen.pop();
-      for (int k = 0; k < 4; k++) {
-        int nx = cur.first + dx[k];
-        int ny = cur.second + dy[k];
-        if (nx < 0 || bX <= nx || ny < 0 || bY <= ny) {
-          cout << visS[cur.first][cur.second] << '\n';
-          escape = true;
+        if(nx < 0 || nx >= n || ny < 0 || ny >= m){
+          flag = true;
+          cout << disS[cur.first][cur.second] + 1 << '\n';
           break;
         }
-        if (board[nx][ny] == -1) continue;
-        if (visS[nx][ny]) continue;
-        if (visF[nx][ny] != 0 && visF[nx][ny] <= visS[cur.first][cur.second] + 1) continue;
-        visS[nx][ny] = visS[cur.first][cur.second] + 1;
-        Sanguen.push({nx, ny});
+        if(board[nx][ny] == '#' || board[nx][ny] == '*' || disS[nx][ny] != -1) continue;
+        if(disF[nx][ny] != -1 && disF[nx][ny] <= disS[cur.first][cur.second] + 1) continue;
+
+        disS[nx][ny] = disS[cur.first][cur.second] + 1;
+        qs.push({nx,ny});
       }
     }
-    if (!escape) cout << "IMPOSSIBLE\n";
-  }
 
-  return 0;
+    if(!flag) cout << "IMPOSSIBLE\n";
+  }
 }

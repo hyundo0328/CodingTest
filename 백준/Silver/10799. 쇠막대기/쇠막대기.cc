98f48ab0bbfb24f01eb013d581da029b62d30d62
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-19 18:44:32 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -5,21 +5,20 @@ int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  stack<int> s;
-	string str;
-	cin >> str;
-	
-	int cnt = 0;
-	for (int i=0; i<str.size(); i++){
-		if (str[i] == '(' && str[i+1] == ')'){
-			cnt += s.size();
-			i++;
-		} else if (str[i] == '('){
-			s.push(i);
-		} else if (str[i] == ')'){
-			cnt++;
-			s.pop();
-		}
-	}
-	cout << cnt;
+  string stick; cin >> stick;
+  stack<char> st;
+  
+  int total = 0;
+  for(int i=0; i<stick.size(); i++){
+    if(stick[i] == '(') st.push(stick[i]);
+    else if(stick[i-1] == '(' && stick[i] == ')'){
+      st.pop();
+      total += st.size();
+    } else{
+      total++;
+      st.pop();
+    }
+  }
+
+  cout << total;
 }

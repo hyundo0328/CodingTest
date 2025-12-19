cff3db4a0298de8304a9a6e2f7670516daea5486
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-19 17:34:43 +0900

    [Gold V] Title: 토마토, Time: 112 ms, Memory: 11904 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -1,72 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dx[6] = {0, 0, 1, -1, 0, 0};
-int dy[6] = {1, -1, 0, 0, 0, 0};
-int dz[6] = {0, 0, 0, 0, 1, -1};
+int dx[6] = {1,0,-1,0,0,0};
+int dy[6] = {0,1,0,-1,0,0};
+int dz[6] = {0,0,0,0,1,-1};
 
 int board[102][102][102];
 int dis[102][102][102];
-
 int m, n, h;
 
-int main(void){
+int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   cin >> m >> n >> h;
 
-  queue<pair<pair<int, int>, int>> q;
   memset(dis, -1, sizeof(dis));
+  queue<tuple<int,int,int>> q;
 
-  for(int k=1; k<=h; k++){
-    for(int i=1; i<=n; i++){
-      for(int j=1; j<=m; j++){
-        cin >> board[i][j][k];
+  // board 등록, 익은 토마토 큐에 저장
+  for(int height = 0; height < h; height++){
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < m; j++){
+        cin >> board[i][j][height];
 
-        if(board[i][j][k] == 1) {
-          q.push({{i,j}, k});
-          dis[i][j][k] = 0;
+        if(board[i][j][height] == 1){
+          dis[i][j][height] = 0;
+          q.push({i,j,height});
         }
       }
     }
   }
 
-  int day = 0;
   while(!q.empty()){
     auto cur = q.front(); q.pop();
-    int x = cur.first.first;
-    int y = cur.first.second;
-    int z = cur.second;
-    // cout << "시작 좌표 x, y, z: " << x << " " << y << " " << z << " " << dis[x][y][z] << '\n';
+    int curX, curY, curZ;
+    tie(curX, curY, curZ) = cur;
 
-    for(int dir = 0; dir < 6; dir++){
-      int nx = x + dx[dir];
-      int ny = y + dy[dir];
-      int nz = z + dz[dir];
+    for(int dir=0; dir<6; dir++){
+      int nx = curX + dx[dir];
+      int ny = curY + dy[dir];
+      int nz = curZ + dz[dir];
 
-      /* 예외 */
-      if(nx < 1 || nx > n || ny < 1 || ny > m || nz < 1 || nz > h) continue; // 상자 벗어남
-      if(dis[nx][ny][nz] > -1 || board[nx][ny][nz] != 0) continue; // 이미 체크 or 익음
-      
-      dis[nx][ny][nz] = dis[x][y][z] + 1;
-      // cout << "x, y, z: " << nx << " " << ny << " " << nz << " " << dis[nx][ny][nz] << '\n';
-      day = max(day, dis[nx][ny][nz]);
-      q.push({{nx,ny},nz});
+      if(nx < 0 || ny < 0 || nz < 0 || nx >=n || ny >= m || nz >= h) continue;
+      if(board[nx][ny][nz] == -1 || dis[nx][ny][nz] != -1) continue;
+
+      dis[nx][ny][nz] = dis[curX][curY][curZ] + 1;
+      q.push({nx,ny,nz});
     }
   }
 
-  for(int k=1;k<=h;k++){
-    for(int i=1;i<=n;i++){
-      for(int j=1;j<=m;j++){
-        if(board[i][j][k] == 0 && dis[i][j][k] == -1) {
+  int mx = 0;
+  for(int height=0; height<h; height++){
+    for(int i=0; i<n; i++){
+      for(int j=0; j<m; j++){
+        if(board[i][j][height] != -1 && dis[i][j][height] == -1){
           cout << -1;
           return 0;
         }
+        mx = max(mx, dis[i][j][height]);
       }
     }
   }
 
-  cout << day;
-  return 0;
+  cout << mx;
 }

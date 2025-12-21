ff6b1a3949481472e459d19339e4bdf7e8347d65
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-21 14:57:36 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -2,64 +2,50 @@
 using namespace std;
 
 int dx[4] = {1,0,-1,0};
-int dy[4] = {0,-1,0,1};
-
-int board[27][27];
+int dy[4] = {0,1,0,-1};
+string board[27];
 bool vis[27][27];
+int n, width = 1, cnt = 0;
+vector<int> vec;
+queue<pair<int,int>> q;
 
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int n;
   cin >> n;
+  for(int i=0; i<n; i++) cin >> board[i];
 
-  for(int i=0;i<n;i++){
-    string s;
-    cin >> s;
-    for(int j=0;j<n;j++){
-      board[i][j] = s[j] - '0';
-    }
-  }
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+      if(board[i][j] == '0' || vis[i][j]) continue;
 
-  int count = 0;
-  vector<int> ans;
-
-  for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
-      if(board[i][j] == 0 || !!vis[i][j]) continue;
-
-      queue<pair<int,int>> Q;
-      Q.push({i,j});
+      cnt++;
       vis[i][j] = true;
-      
-      count++;
-      int width = 1;
+      q.push({i,j});
 
-      while(!Q.empty()){
-        auto cur = Q.front(); Q.pop();
+      while(!q.empty()){
+        auto cur = q.front(); q.pop();
 
         for(int dir=0; dir<4; dir++){
           int nx = cur.first + dx[dir];
           int ny = cur.second + dy[dir];
 
           if(nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-          if(!!vis[nx][ny] || board[nx][ny] == 0) continue;
-
-          Q.push({nx,ny});
-          vis[nx][ny] = true;
+          if(board[nx][ny] == '0' || vis[nx][ny]) continue;
+          
           width++;
+          vis[nx][ny] = true;
+          q.push({nx, ny});
         }
       }
 
-      ans.push_back(width);
+      vec.push_back(width);
+      width = 1;
     }
   }
 
-  sort(ans.begin(), ans.end());
-
-  cout << count << '\n';
-  for(auto width:ans){
-    cout << width << '\n';
-  }
+  sort(vec.begin(), vec.end());
+  cout << cnt << '\n';
+  for(int wid:vec) cout << wid << '\n';
 }

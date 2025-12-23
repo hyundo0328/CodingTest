b1644515d43bcfc47c3b7d835d490aa0e7dc3c3b
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-24 00:15:28 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,21 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-long long a, b, c, k;
+long long multiple(int a, int b, int c){
+  if(b == 1) return a % c;
 
-long long power(long long b) {
-	if (b == 0) return 1;
-	if (b == 1) return a % c;
-	
-	k = power(b / 2) % c;
-	if (b % 2 == 0) return k * k % c;
-    
-	return k * k % c * a % c;
+  long long val = multiple(a, b/2, c);
+  val = val * val % c;
+
+  if(b % 2 == 0) return val;
+  else return val * a % c;
 }
 
-int main(void) {
-	cin >> a >> b >> c; 
-	cout << power(b);
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-	return 0;
+  long long a, b, c;
+  cin >> a >> b >> c;
+
+  cout << multiple(a,b,c);
 }

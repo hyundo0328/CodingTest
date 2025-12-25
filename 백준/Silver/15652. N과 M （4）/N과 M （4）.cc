0ecc8dedae89e93b893539b37c8052d9bbcbca6e
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-26 00:22:41 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -4,30 +4,25 @@ using namespace std;
 int n, m;
 int arr[9];
 
-void func(int num){
-  if(num == m){
-    for(int i=0; i<m; i++){
-      cout << arr[i] << ' ';
-    }
-
+void func(int k){
+  if(k == m){
+    for(int i=0; i<m; i++) cout << arr[i] << ' ';
     cout << '\n';
+
     return;
   }
 
   int start;
-  if(num != 0) {
-    start = arr[num-1];
-  } else {
-    start = 1;
-  }
+  if(k == 0) start = 1;
+  else start = arr[k-1];
 
   for(int i=start; i<=n; i++){
-    arr[num] = i;
-    func(num+1);
+    arr[k] = i;
+    func(k+1);
   }
 }
 
-int main(void){
+int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 

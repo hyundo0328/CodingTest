7168b5ec97b9a9e45efa726f7ba822079f3bc5b0
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2025-12-27 14:15:48 +0900

    [Silver III] Title: N과 M (5), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -3,39 +3,34 @@ using namespace std;
 
 int n, m;
 int arr[9];
+int answer[9];
 bool isUsed[9];
-int num_arr[9];
 
-void func(int num){
-  if(num == m){
-    for(int i=0; i<m; i++){
-      cout << arr[i] << ' ';
-    }
-
+void func(int k){
+  if(k == m){
+    for(int i=0; i<m; i++) cout << answer[i] << ' ';
     cout << '\n';
+
     return;
   }
 
-  for(int i=1; i<=n; i++){
+  for(int i=0; i<n; i++){
     if(!isUsed[i]){
-      arr[num] = num_arr[i];
-      isUsed[i] = 1;
-
-      func(num+1);
-
-      isUsed[i] = 0;
+      answer[k] = arr[i];
+      isUsed[i] = true;
+      func(k+1);
+      isUsed[i] = false;
     }
   }
 }
 
-int main(void){
+int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   cin >> n >> m;
-  for(int i=1;i<=n;i++){
-    cin >> num_arr[i];
-  }
-  sort(num_arr+1, num_arr+n+1);
+  for(int i=0; i<n; i++) cin >> arr[i];
+  sort(arr, arr+n);
+
   func(0);
 }

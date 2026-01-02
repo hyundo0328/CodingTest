099b0bfb7b0dbb3ce2270adc4e181e7b38d3745a
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2026-01-03 00:41:02 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,64 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, max_area, height;
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
 int board[102][102];
 bool vis[102][102];
+int n, mx = 0, maxH = 0;
 
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, -1, 0, 1};
-
-void bfs(int i, int j, int limit){
-  queue<pair<int,int>> q;
-  q.push({i,j});
-  vis[i][j] = true;
-
+void bfs(queue<pair<int,int>>& q, int h){
   while(!q.empty()){
     auto cur = q.front(); q.pop();
 
-    for(int dir=0;dir<4;dir++){
-      int nx = dx[dir] + cur.first;
-      int ny = dy[dir] + cur.second;
+    for(int dir=0; dir<4; dir++){
+      int nx = cur.first + dx[dir];
+      int ny = cur.second + dy[dir];
 
-      if(nx < 0 || ny < 0 || nx > n || ny > n) continue;
-      if(vis[nx][ny] || board[nx][ny] < limit) continue;
-
+      if(nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+      if(board[nx][ny] <= h || vis[nx][ny]) continue;
+      
       vis[nx][ny] = true;
       q.push({nx, ny});
     }
   }
 }
 
+int area(int h){
+  int cnt = 0;
+
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+      if(board[i][j] <= h || vis[i][j]) continue;
+
+      cnt++;
+      queue<pair<int,int>> q;
+      q.push({i,j});
+
+      bfs(q, h);
+    }
+  }
+
+  return cnt;
+}
+
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
-
+  
   cin >> n;
-
-  for(int i=1;i<=n;i++){
-    for(int j=1;j<=n;j++){
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
       cin >> board[i][j];
-      height = max(height, board[i][j]);
+      maxH = max(maxH, board[i][j]);
     }
   }
 
-  for(int h=1; h<=height; h++){
-    for(int i=1;i<=n;i++){
-      fill(vis[i]+1, vis[i]+n+1, false);
-    }
-    
-    int area = 0;
-    for(int i=1;i<=n;i++){
-      for(int j=1;j<=n;j++){
-        if(vis[i][j] || board[i][j] < h) continue;
+  for(int h=0; h<maxH; h++){
+    memset(vis, false, sizeof(vis)); // 방문 데이터 초기화
 
-        bfs(i,j,h);
-        area++;
-      }
-    }
-
-    max_area = max(max_area, area);
+    int cnt = area(h);
+    mx = max(mx, cnt);
   }
 
-  cout << max_area;
+  cout << mx;
 }

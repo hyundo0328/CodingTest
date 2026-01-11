fc3ef55ed30423d532e7f00f14ec666fc50510f3
Park Hyundo <117895948+hyundo0328@users.noreply.github.com>
2026-01-11 23:19:39 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 456 ms, Memory: 50936 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
+int board[1002][1002];
+int dis[1002][1002][11];
+int n, m, k;
+
+int bfs(){
+  queue<tuple<int, int, int>> q;
+  q.push({0, 0, 0});
+
+  while(!q.empty()){
+    int x, y, broken;
+    tie(x, y, broken) = q.front(); q.pop();
+
+    if(x == n - 1 && y == m - 1) return dis[x][y][broken];
+
+    for(int dir = 0; dir < 4; dir++){
+      int nx = x + dx[dir];
+      int ny = y + dy[dir];
+
+      if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+
+      if(board[nx][ny] == 0 && dis[nx][ny][broken] == 0){
+        dis[nx][ny][broken] = dis[x][y][broken] + 1;
+        q.push({nx, ny, broken});
+      }
+      if(broken + 1 <= k && board[nx][ny] == 1 && dis[nx][ny][broken + 1] == 0){
+        dis[nx][ny][broken+1] = dis[x][y][broken] + 1;
+        q.push({nx, ny, broken + 1});
+      }
+    }
+  }
+
+  return -1;
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n >> m >> k;
+  for(int i=0; i<n; i++){
+    string tmp; cin >> tmp;
+    for(int j=0; j<m; j++){
+      board[i][j] = tmp.at(j) - '0';
+    }
+  }
+
+  dis[0][0][0] = 1;
+  cout << bfs();
+}
